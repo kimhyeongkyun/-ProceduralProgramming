0df335950b8a80b94a9109752a35b380a4dcfdc6
ask03 <ask03@DESKTOP-LMP9MU8>
2019-03-26 12:56:01 +0900

    절차적사고프로그래밍

---
+++ [0304]60152179_김형균/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+		
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int message = sc.nextInt();
+		System.out.println(message+message);
+		//object, message, object
+	
+	}
+
+}
